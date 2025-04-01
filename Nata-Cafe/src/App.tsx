1026ff00b119333c1b30f7a60de4d5ad801a78b2
Naibafe <fabian.tworo@wp.pl>
2025-04-01 18:21:30 +0200

    zmiana

--- Nata-Cafe/src/App.tsx
+++ Nata-Cafe/src/App.tsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Nav />
-    </>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/kontakt" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- Nata-Cafe/src/components/Nav.tsx
+++ Nata-Cafe/src/components/Nav.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import Logo from "../assets/logo.png";
 
 export default function Nav() {
@@ -5,6 +6,17 @@ export default function Nav() {
     <div className="navbar">
       <img src={Logo} alt="logo" className="nav-logo" />
       <div className="logo-sign">Nata - Cafe</div>
+      <div className="nav-links">
+        <NavLink to="/kontakt" className="link-element">
+          <p>Kontakt</p>
+        </NavLink>
+        <NavLink to="/kontakt" className="link-element">
+          <p>Kontakt</p>
+        </NavLink>
+        <NavLink to="/kontakt" className="link-element">
+          <p>Kontakt</p>
+        </NavLink>
+      </div>
     </div>
   );
 }
